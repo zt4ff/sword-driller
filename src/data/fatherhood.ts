ace4f83ad0d816e7c195daf0d087302c40ad2d82
Kayode Oluwasegun <segunkayode00@gmail.com>
2025-09-09 10:54:59 +0100

    feat: fix fatherhood quotes

--- src/data/fatherhood.ts
+++ src/data/fatherhood.ts
@@ -6,36 +6,66 @@ export const fatherhoodQuotes = [
   },
   {
     quote:
-      "As a father has compassion on his children, so the Lord has compassion on those who fear him;",
-    reference: "Psalm 103:13",
+      "And a voice from heaven said, “This is my Son, whom I love; with him I am well pleased.”",
+    reference: "Matthew 3:17",
   },
   {
     quote:
-      "See what great love the Father has lavished on us, that we should be called children of God! And that is what we are!",
-    reference: "1 John 3:1",
-  },
-  {
-    quote: "for your Father knows what you need before you ask him.",
-    reference: "Matthew 6:8",
+      "That you may be children of your Father in heaven. He causes his sun to rise on the evil and the good, and sends rain on the righteous and the unrighteous.",
+    reference: "Matthew 5:45",
   },
   {
     quote:
-      "If you, then, though you are evil, know how to give good gifts to your children, how much more will your Father in heaven give good gifts to those who ask him!",
-    reference: "Matthew 7:11",
+      "This, then, is how you should pray: “Our Father in heaven, hallowed be your name.”",
+    reference: "Matthew 6:9",
   },
   {
     quote:
-      "But when the set time had fully come, God sent his Son, born of a woman, born under the law, to redeem those under the law, that we might receive adoption to sonship.",
-    reference: "Galatians 4:4-5",
+      "Jesus replied, “Blessed are you, Simon son of Jonah, for this was not revealed to you by flesh and blood, but by my Father in heaven.”",
+    reference: "Matthew 16:17",
   },
   {
     quote:
-      "The Spirit you received does not make you slaves, so that you live in fear again; rather, the Spirit you received brought about your adoption to sonship. And by him we cry, “Abba, Father.”",
-    reference: "Romans 8:15",
+      "Then Jesus came to them and said, “All authority in heaven and on earth has been given to me.”",
+    reference: "Matthew 28:18",
   },
   {
     quote:
-      "Yet to all who did receive him, to those who believed in his name, he gave the right to become children of God—",
-    reference: "John 1:12",
+      "The beginning of the good news about Jesus the Messiah, the Son of God.",
+    reference: "Mark 1:1",
+  },
+  {
+    quote:
+      "“Why were you searching for me?” he asked. “Didn’t you know I had to be in my Father’s house?”",
+    reference: "Luke 2:49",
+  },
+  {
+    quote:
+      "Jesus called out with a loud voice, “Father, into your hands I commit my spirit.” When he had said this, he breathed his last.",
+    reference: "Luke 23:46",
+  },
+  {
+    quote:
+      "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.",
+    reference: "John 3:16",
+  },
+  {
+    quote: "I and the Father are one.",
+    reference: "John 10:30",
+  },
+  {
+    quote:
+      "My Father’s house has many rooms; if that were not so, would I have told you that I am going there to prepare a place for you?",
+    reference: "John 14:2",
+  },
+  {
+    quote:
+      "Jesus answered, “I am the way and the truth and the life. No one comes to the Father except through me. If you really know me, you will know my Father as well. From now on, you do know him and have seen him.”",
+    reference: "John 14:6-7",
+  },
+  {
+    quote:
+      "Don’t you believe that I am in the Father, and that the Father is in me? The words I say to you I do not speak on my own authority. Rather, it is the Father, living in me, who is doing his work.",
+    reference: "John 14:10",
   },
 ];
